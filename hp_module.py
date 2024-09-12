0e9e4f6693c28be7114482e494a5b4cacbd28c7e
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2024-09-12 20:48:42 +0900

    check_can_healとcheck_zero_hpの追加

--- hp_module.py
+++ hp_module.py
@@ -1,3 +1,22 @@
 class hp():
-    def __init__(self):
+    def __init__(self, max_hp:int, current_hp:int):
+        self.max_hp = max_hp
+        self.current_hp = current_hp
+
+    def heal_hp(self, heal_amount:int):
         pass
+
+    def receive_damage(self, damage_amount:int):
+        pass
+
+    def check_can_heal(self):
+        isMax = False
+        if self.current_hp == self.max_hp:
+            isMax = True
+        return isMax
+
+    def check_zero_hp(self):
+        isZero = False
+        if self.current_hp == 0:
+            isZero = True
+        return isZero
