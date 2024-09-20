a09dd581b2b0ae0dd7e769413e9c95cf9eb7b9c2
j.lunax4300 <j.lunax4300@gmail.com>
2024-09-20 17:19:16 +0900

    playerモジュールの独立とプレイヤーパラメータ初期値を定数化

---
+++ player_module.py
@@ -0,0 +1,62 @@
+import entity_module
+
+PLAYER_INITIAL_ATTACK = 10
+PLAYER_INITIAL_DEFENSE = 10
+PLAYER_INITIAL_HP = 10
+PLAYER_INITIAL_MP = 10
+PLAYER_INITIAL_ACTWEIGHT = 10
+
+#playerクラス
+class player(entity_module.entity):
+    #初期化
+    def __init__(self,
+        name,
+        remaining,
+        ):
+        super().__init__(
+            PLAYER_INITIAL_ATTACK,
+            PLAYER_INITIAL_DEFENSE,
+            PLAYER_INITIAL_HP,
+            PLAYER_INITIAL_MP,
+            PLAYER_INITIAL_ACTWEIGHT,
+            )  #親クラスのコンストラクタを呼び出し
+        self.__name = name
+        self.__remaining = remaining
+    
+    #空腹値を変更するメソッド
+    def decrease_hunger():
+        pass
+
+    #name参照メソッド
+    def get_name(self):
+        return self.__name
+
+    #remaining参照メソッド
+    def get_remaining(self):
+        return self.__remaining
+    
+    #攻撃力更新メソッド
+    def update_attack_bybp(self, bp:int):
+        self.__attack += bp
+
+    #防御力更新メソッド
+    def update_defense_bybp(self, bp:int):
+        self.__defense += bp 
+
+    #hp更新メソッド
+    def update_hp_bybp(self, bp:int):
+        self.__hp.change_by_bp(bp)
+
+    #mp更新メソッド
+    def update_defense_bybp(self, bp:int):
+        self.__mp.change_by_bp(bp)
+
+    #行動力更新メソッド
+    def update_act_weight(self, bp:int):
+        update_value = self.__actweight - bp
+        #行動力が3以下にならないときのみ更新する
+        if update_value >= 3:
+            self.__actweight = update_value
+            return True
+        else:
+            return False
