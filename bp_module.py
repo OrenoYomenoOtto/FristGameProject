995b8a5b3145c9f34163fa84f49a3a84df096a0e
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2024-09-20 15:58:56 +0900

    bpモジュールの作成開始

---
+++ bp_module.py
@@ -0,0 +1,21 @@
+class Bp():
+    def __init__(self,total_point):
+        self.total_point = total_point
+
+    def define_point(self):
+        pass
+
+    def define_hp(self):
+        pass
+
+    def define_mp(self):
+        pass
+
+    def define_atk(self):
+        pass
+
+    def define_def(self):
+        pass
+
+    def define_sw(self):
+        pass
