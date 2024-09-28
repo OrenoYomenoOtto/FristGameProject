0f72426b5b413414ddd476d4ad55c69f1d16dbd4
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2024-09-28 19:05:12 +0900

    マネージャでBPの割り振りができるように追記した

--- bp_manager.py
+++ bp_manager.py
@@ -1,37 +1,111 @@
 import bp_module as bp_md
 
+def check_N(point):
+    isNatural_num = True
+    if(point<0):
+        isNatural_num = False
+        print("自然数を入力して下さい")
+    return isNatural_num
+
+def check_enough_point(point):
+    isEnough_point = True
+    if(BP.get_rest_point()<point):
+        isEnough_point = False
+        print("ポイントが足りません")
+    return isEnough_point
+
+def check_range_07(point):
+    isRange = True
+    if(point<0 or point>7):
+        isRange = False
+        print("範囲内の数値を入力してください")
+    return isRange
+
+
 total_point = 100
-
-hp_p =  0
-mp_p = 0
-atk_p = 0
-def_p = 0
-sw_p = 0
-Answer = "n"
-
+bp_list = ("SW","HP","MP","ATK","DEF")
+partition_point = [0,0,0,0,0]
 
 BP = bp_md.bp(total_point)
-ex_txt = ("BPはプレイヤーのステータスを強化できるポイントです。\nBPで強化できるステータスはHP、MP、攻撃力(ATK)、防御力(DEF)、行動の重み(SW)の5つが存在します。\n"
-          "HP、MP、ATK、DEFは総量内であれば好きに変更可能です。ただし、SWは7から0の範囲で強化可能です。\n")
+ex_txt = ("BPはプレイヤーのステータスを強化できるポイントです。\n振り分けた値がプレイヤーのステータスになります。\nBPで強化できるステータスはHP、MP、攻撃力(ATK)、防御力(DEF)、行動の重み(SW)の5つが存在します。\nHP、MP、ATK、DEFは総量内であれば好きに変更可能です。ただし、SWは7から0の範囲で強化可能です。\n")
 
+while(True):
+    print("BPを振り分けましょう。振り分けた値がプレイヤーのステータスになります。\nSW、HP、MP、ATK、DEFの5つにBPを振り分けてください。\n今回のBPは"+str(BP.get_total_point())+"です。\n各項目は半角数字で入力してください")
 
-print("BPを振り分けましょう。\n振り分けた値がプレイヤーのステータスになります。\nHP、MP、ATK、DEF、SWの5つにBPを振り分けてください。")
-print("今回のBPは"+str(total_point)+"です。\n")
-print("HPに振り分けてください。(残りBP："+str(total_point)+")")
-hp_p = input("数値を入力：")
-print("MPに振り分けてください。(残りBP："+str(total_point)+")")
-mp_p = input("数値を入力：")
-print("ATKに振り分けてください。(残りBP："+str(total_point)+")")
-atk_p = input("数値を入力：")
-print("DEFに振り分けてください。(残りBP："+str(total_point)+")")
-def_p = input("数値を入力：")
-print("SWに振り分けてください。範囲は0～7です。(残りBP："+str(total_point)+")")
-sw_p = input("数値を入力：")
+    while(True):
+        print("SWにBPを振り分けてください。(残り:"+str(BP.get_rest_point())+")")
+        print("SWの範囲は0～7です")
+        tmp = input("数値を入力")
+        try:
+            point = int(tmp)
+        except ValueError:
+            print("数値以外が入力されました。半角数字を入力してください")
+            continue
+        if(check_N(point)==True and check_range_07(point)==True and check_enough_point(point)==True):
+            BP.set_rest_point(BP.get_rest_point() - point)
+            partition_point[0] = point
+            break
+    
+    while(True):
+        print("HPにBPを振り分けてください。(残り:"+str(BP.get_rest_point())+")")
+        tmp = input("数値を入力")
+        try:
+            point = int(tmp)
+        except ValueError:
+            print("数値以外が入力されました。半角数字を入力してください")
+            continue
+        if(check_N(point)==True and check_enough_point(point)==True):
+            BP.set_rest_point(BP.get_rest_point() - point)
+            partition_point[1] = point
+            break
 
-print("=================================")
-print("HP："+str(hp_p)+"　MP："+str(mp_p)+"　ATK："+str(atk_p)+"　DEF："+str(def_p)+"　SW："+str(sw_p))
-if(BP>0):
-    Answer = input("BPが残っていますがよろしいですか？(Y/n)")
-    if(Answer == n):
+    while(True):
+        print("MPにBPを振り分けてください。(残り:"+str(BP.get_rest_point())+")")
+        tmp = input("数値を入力")
+        try:
+            point = int(tmp)
+        except ValueError:
+            print("数値以外が入力されました。半角数字を入力してください")
+            continue
+        if(check_N(point)==True and check_enough_point(point)==True):
+            BP.set_rest_point(BP.get_rest_point() - point)
+            partition_point[2] = point
+            break
 
-Answer=input("BPの配分はこれでよろしいですか？(Y/n)")
+    while(True):
+        print("ATKにBPを振り分けてください。(残り:"+str(BP.get_rest_point())+")")
+        tmp = input("数値を入力")
+        try:
+            point = int(tmp)
+        except ValueError:
+            print("数値以外が入力されました。半角数字を入力してください")
+            continue
+        if(check_N(point)==True and check_enough_point(point)==True):
+            BP.set_rest_point(BP.get_rest_point() - point)
+            partition_point[3] = point
+            break
+
+    while(True):
+        print("DEFにBPを振り分けてください。(残り:"+str(BP.get_rest_point())+")")
+        tmp = input("数値を入力")
+        try:
+            point = int(tmp)
+        except ValueError:
+            print("数値以外が入力されました。半角数字を入力してください")
+            continue
+        if(check_N(point)==True and check_enough_point(point)==True):
+            BP.set_rest_point(BP.get_rest_point() - point)
+            partition_point[4] = point
+            break
+
+    print("=================================")
+    for i in range(len(partition_point)):
+        print(bp_list[i]+":"+str(partition_point[i]))
+    print("=================================")
+    print("ステータスはこれでいいですか？(Y or AnyKey)")
+    check_status = input()
+    if(check_status == "Y"):
+        break
+    else:
+        BP.reset_point()
+        pass
--- bp_module.py
+++ bp_module.py
@@ -1,12 +1,19 @@
 class bp():
-    def __init__(self,total_point):
-        self.total_point = total_point
+    def __init__(self,total_point:int):
+        self.__total_point = total_point
+        self.__rest_point = total_point
 
-    def define_hp(self):
-        pass
+    def get_total_point(self):
+        return self.__total_point
+    
+    def get_rest_point(self):
+        return self.__rest_point
+    
+    def set_rest_point(self,num):
+        self.__rest_point = num
 
-    def define_mp(self):
-        pass
+    def reset_point(self):
+        self.__rest_point = self.__total_point
 
     def define_atk(self):
         pass
@@ -14,8 +21,11 @@ class bp():
     def define_def(self):
         pass
 
-    def define_sw(self):
+    def define_hp(self):
         pass
 
-    def define_point(self):
+    def define_mp(self):
+        pass
+
+    def define_sw(self):
         pass
