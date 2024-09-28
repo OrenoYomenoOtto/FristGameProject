e37e48c53fe9d0f1e26c6cc01c457df8aaf5cf17
j.lunax4300 <j.lunax4300@gmail.com>
2024-09-28 16:19:48 +0900

    ドロップアイテム、金銭の実装

--- enemy_module.py
+++ enemy_module.py
@@ -1,13 +1,17 @@
 import entity_module
+import random
 
 #playerクラス
 class player(entity_module.entity):
     #初期化
-    def __init__(self,
+    def __init__(self,drop_item, drop_money, item_drop_probabily
         ):
         super().__init__(
             )  #親クラスのコンストラクタを呼び出し
-        
+        self.__drop_item = drop_item
+        self.__drop_money = drop_money
+        self.__item_drop_probabily = item_drop_probabily
+
     #装備品(プレイヤーのみ)・状態異常(追加予定)を加味して真の攻撃力を返すメソッド
     #現時点で未実装のため、素の攻撃力を返す
     def get_true_defense(self):
@@ -17,3 +21,18 @@ class player(entity_module.entity):
     #現時点で未実装のため、素の防御力を返す
     def get_true_defense(self):
         return self.__defense
+    
+    #アイテムを落とすかを返すメソッド
+    def does_drop_money(self):
+        if random.random() < self.__item_drop_probabily:
+            return True
+        else:
+            return False
+        
+    #アイテムIDを返す
+    def get_drop_itemid(self):
+        return self.__drop_item
+    
+    #お金を返す
+    def get_drop_money(self):
+        return self.__drop_money
